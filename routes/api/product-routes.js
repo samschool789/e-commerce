e059715c7368f6de89ede01d422dcacc89e22052
samschool789@gmail.com <samschool789@gmail.com>
2022-06-17 12:07:59 -0500

    route changes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,32 +8,51 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // find all products
 // be sure to include its associated Category and Tag data
 
-router.get("/", async (req, res) => {
-  try {
-    const productData = await Product.findAll({
-      include: [
-        {
-          model: Category,
-          attributes:['id', 'category_name']
-        },
-        {
-          model: Tag,
-          attributes:['id', 'tag_name']
-
-        },
-      ],
+router.get("/", (req, res) => {
+  Product.findAll({
+    attributes: ["id", "product_name", "price", "stock"],
+    /* include: [{
+              model: Category,
+              attributes: ['category_name']
+          },
+          {
+              model: Tag,
+              attributes: ['tag_name']
+          }
+      ]*/
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-    res.status(200).json(productData);
-  } catch (error) {
-    res.status(500).json(error);
-  }
 });
 
 // get one product
+// find a single product by its `id`
+// be sure to include its associated Category and Tag data
 router.get("/:id", (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-  
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "product_name", "price", "stock"],
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
+  })
+    .then((dbProductData) => res.json(dbProductData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
